cede3f64a9fd58450a3fb7bda34279af07829497
aca7 <aca7@ug3c33.windows.cin.ufpe.br>
2019-05-24 08:48:46 -0300

    Prato corrigido

---
+++ AlmirSimulator/src/pratos/Prato.java
@@ -0,0 +1,14 @@
+package pratos;
+
+public abstract class Prato {
+	private String nome;
+
+	public Prato(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+}
