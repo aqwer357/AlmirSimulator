617f84e9cf79c9322c14b6f7ae7c6876eab6d838
Gabriel Meireles <48731050+GabrielUTR@users.noreply.github.com>
2019-05-27 09:34:32 -0300

    Atualizando

--- AlmirSimulator/src/fornecedores/Fornecedor.java
+++ AlmirSimulator/src/fornecedores/Fornecedor.java
@@ -2,11 +2,34 @@ package fornecedores;
 
 public class Fornecedor {
 	private String nome;
-	private Ingredientes ingrediente;
+	private RepositoriosIngredientesArray ingredientes;
+	private int identificador;
 	
-	public Fornecedor(String nome, Ingredientes ingrediente) {
+	public Fornecedor(String nome, RepositoriosIngredientesArray ingredientes, int identificador) {
 		this.nome = nome;
-		this.ingrediente =ingrediente;
+		this.ingredientes =ingredientes;
+		this.identificador = identificador;
 	}
 	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIngrediente(Ingrediente ingrediente) {
+		this.ingrediente = ingrediente;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public Ingrediente getIngrediente() {
+		return this.ingrediente;
+	}
+	public int getIdentificador() {
+		return this.identificador;
+	}
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
 }
--- AlmirSimulator/src/fornecedores/RepositorioFornecedores.java
+++ AlmirSimulator/src/fornecedores/RepositorioFornecedores.java
@@ -1,5 +1,21 @@
 package fornecedores;
 
 public interface RepositorioFornecedores {
+	
+	void inserir(Fornecedor fornecedor) ;
+		
+	
+	
+	void atualizar (Fornecedor fornecedor) ;
+		
+	
+	void remover(int identificador) ;
+		
+	
+	boolean existe(int identificador) ;
+	
 
+	Fornecedor procurar(int identificador) ;
+		
+	}
 }
