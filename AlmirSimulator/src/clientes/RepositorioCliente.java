e33df3dd1c1f8210d6b6f72913d3567d6dcf8e54
gcgm <gcgm@ug3c25.windows.cin.ufpe.br>
2019-05-31 08:47:04 -0300

    Atualizando

--- AlmirSimulator/src/clientes/RepositorioCliente.java
+++ AlmirSimulator/src/clientes/RepositorioCliente.java
@@ -4,7 +4,7 @@ public interface RepositorioCliente {
 
 	boolean existe(String nome) throws ClienteNaoExistenteException;
 	Cliente procurar(String nome);
-	void inserir(Cliente cliente);
+	void inserir(Cliente cliente) throws LimiteAtingidoException;
 	void atualizar(Cliente cliente);
 	void remover(String nome);
 	
--- AlmirSimulator/src/clientes/RepositorioClienteArray.java
+++ AlmirSimulator/src/clientes/RepositorioClienteArray.java
@@ -9,6 +9,20 @@ public class RepositorioClienteArray implements RepositorioCliente {
 		this.indice = 0;
 	}
 	
+	public void inserir(Cliente cliente) throws LimiteAtingidoException {
+		
+		if(this.indice < this.arrayCliente.length) { // verificando se ainda há espaço livre no array
+			this.arrayCliente[indice] = cliente;
+			indice++;
+		}
+		else {
+			LimiteAtingidoException exceção;
+			exceção = new LimiteAtingidoException();
+			throw exceção;
+		}
+		
+	}
+	
 	
 	
 	
