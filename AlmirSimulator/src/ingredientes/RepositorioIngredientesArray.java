39651b42d086052d14f136729c5c328a17c65bf6
Charles Gabriel Carvalho Cristovao <cgcc@ug3c27.windows.cin.ufpe.br>
2019-06-03 09:52:00 -0300

    atualizando repositorio array

--- AlmirSimulator/src/ingredientes/RepositorioIngredientesArray.java
+++ AlmirSimulator/src/ingredientes/RepositorioIngredientesArray.java
@@ -1,5 +1,58 @@
 package ingredientes;
 
 public class RepositorioIngredientesArray {
+	private Ingrediente[] ingredientes = new Ingrediente[1000];
+	private int indice;
 	
+	public RepositorioIngredientesArray() {
+		this.indice = 0;
+	}
+	
+	public void inserir(Ingrediente ingrediente) throws IngredienteJaExisteException  {
+		if (existe(ingrediente.getNome())) {
+			throw new IngredienteJaExisteException();
+		} else {
+			this.ingredientes[indice] = ingrediente;
+			indice++;
+			
+		}
+	}
+	
+	public void atualizar() throws IngredienteNaoEncontradoException, IngredienteJaExisteException {
+		
+	}
+	
+	public void remover(String nome) throws IngredienteNaoEncontradoException {
+		
+	}
+	
+	public Ingrediente procurar(String nome) throws IngredienteNaoEncontradoException {
+		int aux = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (ingredientes[i] != null) {
+				if (ingredientes[i].getNome().contentEquals(nome)) {
+					aux = i;
+					i = 1000;
+				} 
+			} else {
+				throw new IngredienteNaoEncontradoException();
+			}
+		}
+		return this.ingredientes[aux];
+	}
+	
+	public boolean existe(String nome) {
+		boolean existe = false;
+		for (int i = 0; i < 1000; i++) {
+			if (ingredientes[i] != null) {
+				if (ingredientes[i].getNome().contentEquals(nome)) {
+					i = 1000;
+					existe = true;
+				}
+			} else {
+				existe = false;
+			}
+		}
+		return existe;
+	}
 }
