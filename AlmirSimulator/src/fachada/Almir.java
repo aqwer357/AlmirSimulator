c88c10ea67dd8d608291a78376509030ae20635c
Charles Gabriel Carvalho Cristovao <cgcc@WINDOWS.CIN.UFPE.BR>
2019-06-07 09:47:01 -0300

    atualizando cadastroIngredientes

--- AlmirSimulator/src/fachada/Almir.java
+++ AlmirSimulator/src/fachada/Almir.java
@@ -17,40 +17,62 @@ public class Almir {
 	private CadastroIngredientes ingredientes;
 	private CadastroPedidos pedidos;
 	private CadastroPratos pratos;
-	
+
 	// o booleano tipo decide se é lista ou array
 	// ignora a ideia de cima, sergio falou q era má ideia
 	// to fazendo como ele recomendou
-	
+
 	public Almir(RepositorioFornecedores fornecedores, RepositorioIngredientes ingredientes, RepositorioPedidos pedidos, RepositorioPratos pratos) {
 		this.fornecedores = new CadastroFornecedores(fornecedores);
 		this.ingredientes = new CadastroIngredientes(ingredientes);
 		this.pedidos = new CadastroPedidos(pedidos);
 		this.pratos = new CadastroPratos(pratos);
 	}
-	
+
 	//fornecedor
-	
+
 	public void cadastrarFornecedor(Fornecedor fornecedor) throws FornecedorJaCadastradoException {
 		this.fornecedores.cadastrar(fornecedor);
 	}
-	
+
 	public void remover(int identificador) throws FornecedorNaoEncontradoException{
 		this.fornecedores.remover(identificador);
 	}
-	
+
 	public void atualizar(Fornecedor fornecedor) throws FornecedorNaoEncontradoException {
 		this.fornecedores.atualizar(fornecedor);
 	}
-	
+
 	public boolean existe(int identificador) {
-        return this.fornecedores.existe(identificador);
-    }
-	
+		return this.fornecedores.existe(identificador);
+	}
+
 	public Fornecedor procurar(int identificador) throws FornecedorNaoEncontradoException{
 		return this.fornecedores.procurar(identificador);
 	}
-	
+
+	// A partir daqui: ingredientes
+
+	public void cadastrarIngrediente(Ingrediente ingrediente) throws IngredienteJaCadastradoException {
+		this.ingredientes.cadastrar(ingrediente);
+	}
+
+	public void remover(String nome) throws IngredienteNaoEncontradoException{
+		this.ingredientes.remover(nome);
+	}
+
+	public void atualizar(Ingrediente ingrediente) throws IngredienteNaoEncontradoException {
+		this.ingredientes.atualizar(ingrediente);
+	}
+
+	public boolean existe(String nome) {
+		return this.ingredientes.existe(nome);
+	}
+
+	public Ingrediente procurar(String nome) throws IngredienteNaoEncontradoException{
+		return this.ingredientes.procurar(nome);
+	}
+
 	// CHARLES NA HORA DE TU FAZER A TUA PARTE DA FACHADA FAZ O SEGUINTE:
 	// QUANDO TU INSERIR, TU PEGA INGREDIENTE DO REPOSITORIO DE FORNECEDOR
 	// SE NAO TIVER, CRIA UM ERRO
--- AlmirSimulator/src/ingredientes/CadastroIngredientes.java
+++ AlmirSimulator/src/ingredientes/CadastroIngredientes.java
@@ -2,36 +2,46 @@ package ingredientes;
 
 public class CadastroIngredientes {
 	private RepositorioIngredientes ingrediente;
-	
+
 	public CadastroIngredientes (RepositorioIngredientes ingrediente) {
-		
-	this.ingrediente = ingrediente;
-	
+
+		this.ingrediente = ingrediente;
+
 	}
-	
+
 	public void cadastrar(Ingrediente Ingrediente) throws IngredienteJaCadastradoException {
 		if(this.ingrediente.existe(Ingrediente.getNome())) {
-			IngredienteJaCadastradoException e;
-			e = new IngredienteJaCadastradoException();
-			throw e;
+			throw new IngredienteJaCadastradoException();
 		}else {
 			this.ingrediente.inserir(Ingrediente);
 		}
 	}
-	
-	public void remover(String identificador) throws IngredienteNaoEncontradoException{
-		this.ingrediente.remover(identificador);
+
+	public void remover(String nome) throws IngredienteNaoEncontradoException {
+		if (existe(nome)) {
+			this.ingrediente.remover(nome);
+		} else {
+			throw new IngredienteNaoEncontradoException();
+		}
 	}
-	
+
 	public void atualizar(Ingrediente Ingrediente) throws IngredienteNaoEncontradoException {
-		this.ingrediente.atualizar(Ingrediente);
+		if (existe(Ingrediente.getNome())) {
+			this.ingrediente.atualizar(Ingrediente);
+		} else {
+			throw new IngredienteNaoEncontradoException();
+		}
 	}
-	
+
 	public boolean existe(String identificador) {
-        return this.ingrediente.existe(identificador);
-    }
-	
-	public Ingrediente procurar(String identificador) throws IngredienteNaoEncontradoException{
-		return this.ingrediente.procurar(identificador);
+		return this.ingrediente.existe(identificador);
+	}
+
+	public Ingrediente procurar(String nome) throws IngredienteNaoEncontradoException {
+		if (existe(nome)) {
+			return this.ingrediente.procurar(nome);
+		} else {
+			throw new IngredienteNaoEncontradoException();
+		}
 	}
 }
