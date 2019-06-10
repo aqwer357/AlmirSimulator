d2aa31cdbabed61d4e62a968f9dfca4f2fd61311
Gabriel Meireles <48731050+GabrielUTR@users.noreply.github.com>
2019-06-10 09:24:28 -0300

    Update CadastroIngredientes.java

--- AlmirSimulator/src/ingredientes/CadastroIngredientes.java
+++ AlmirSimulator/src/ingredientes/CadastroIngredientes.java
@@ -33,8 +33,8 @@ public class CadastroIngredientes {
 		}
 	}
 	
-	public boolean existe(String identificador) {
-        return this.ingredientes.existe(identificador);
+	public boolean existe(String nome) {
+        return this.ingredientes.existe(nome);
     }
 	
 	public Ingrediente procurar(String nome) throws IngredienteNaoEncontradoException {
