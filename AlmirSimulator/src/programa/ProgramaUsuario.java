2995f1524f98a1de2a5dfdd670697d5f26d12d60
aca7 <aca7@cin.ufpe.br>
2019-06-12 14:39:16 -0300

    Programa vira ProgramaUsuario; Aguardando Gabriel

--- AlmirSimulator/src/programa/ProgramaUsuario.java
+++ AlmirSimulator/src/programa/ProgramaUsuario.java
@@ -9,14 +9,15 @@ import fornecedores.*;
 
 import java.util.*;
 
-public class Programa {
+public class ProgramaUsuario {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FornecedorJaCadastradoException, IngredienteJaCadastradoException {
 		
 		Scanner in = new Scanner(System.in);
 		
 		Almir almirLista = new Almir(new RepositorioFornecedoresLista(), new RepositorioIngredientesLista(), new RepositorioPedidosLista(),
 				new RepositorioPratosLista(), new RepositorioClientesLista()),
+				
 			  almirArray = new Almir(new RepositorioFornecedoresArray(), new RepositorioIngredientesArray(), new RepositorioPedidosArray(),
 						new RepositorioPratosArray(), new RepositorioClientesArray());
 		
@@ -32,11 +33,25 @@ public class Programa {
 			
 			System.out.printf("Quantos ingredientes ele fornece?");
 			int quantIngredientes = in.nextInt();
+			boolean ehEspecial = false;
 			
 			for (int j = 0; j < quantIngredientes; i++) {
 				System.out.printf("\nInsira o nome do ingrediente:");
+				String nomeIngrediente = in.nextLine();
+				
+				System.out.printf("Eh especial? (responda com sim ou nao)");
+				String especial = in.next();
+				
+				if (especial.equals("sim"))
+					ehEspecial = true;
+				else
+					ehEspecial = false;
+				
+				ingredientesFornecedor.inserir(new Ingrediente(nomeIngrediente, ehEspecial));
 				
 			}
+			
+			almirLista.cadastrarFornecedor(new Fornecedor(nomeFornecedor, ingredientesFornecedor, i));
 		}
 	}
 
