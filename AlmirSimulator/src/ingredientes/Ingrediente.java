31d0747af89525d03bb220103fec60a0ebcdeb2a
Gabriel Meireles <48731050+GabrielUTR@users.noreply.github.com>
2019-06-12 11:27:15 -0300

    Update Ingrediente.java

--- AlmirSimulator/src/ingredientes/Ingrediente.java
+++ AlmirSimulator/src/ingredientes/Ingrediente.java
@@ -5,16 +5,12 @@ public class Ingrediente {
 	private boolean existe;
 	private boolean especial;
 	
-	public Ingrediente(String nome, boolean existe, boolean especial) {
+	public Ingrediente(String nome, boolean especial) {
 		this.nome = nome;
-		this.existe = existe;
 		this.especial = especial;
 	}
 	public String getNome() {
 		return this.nome;
-	}
-	public boolean getExiste() {
-		return this.existe;
 	}
 	public boolean getTipo() {
 		return this.especial;
