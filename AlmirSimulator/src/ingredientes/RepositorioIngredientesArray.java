39a5a2c091b69cb23577d30a078d57426b6b0770
Gabriel Meireles <48731050+GabrielUTR@users.noreply.github.com>
2019-06-16 13:22:03 -0300

    Update RepositorioIngredientesArray.java

--- AlmirSimulator/src/ingredientes/RepositorioIngredientesArray.java
+++ AlmirSimulator/src/ingredientes/RepositorioIngredientesArray.java
@@ -21,65 +21,66 @@ public class RepositorioIngredientesArray implements RepositorioIngredientes {
 	public int getIndice() {
 		return this.indice;
 	}
+	public int getIndiceOf(String nome) {
+		int indice = 0;
+		
+		for(int i=0;i<this.indice;i++) {
+			if(this.ingredientes[i].getNome().contentEquals(nome)) {
+				indice = i;
+				i = this.indice;
+			}
+		}
+		return indice;
+	}
 
 	public void atualizar(Ingrediente ingrediente) throws IngredienteNaoEncontradoException {
-		Ingrediente ingredienteAlvo;
-		if (existe(ingrediente.getNome())) {
-			ingredienteAlvo = procurar(ingrediente.getNome());
-			this.remover(ingredienteAlvo.getNome());
-			this.inserir(ingrediente);
+		if(this.existe(ingrediente.getNome())) {
+			
+			int i = this.getIndiceOf(ingrediente.getNome());
+			this.ingredientes[i] = ingrediente;
+			
 		} else {
 			throw new IngredienteNaoEncontradoException();
 		}
 	}
 
 	public void remover(String nome) throws IngredienteNaoEncontradoException {
-		Ingrediente aux;
-		for (int i = 0; i < 1000; i++) {
-			if (i < 999) {
-				if (nome.contentEquals(ingredientes[i].getNome()) && ingredientes[i + 1] != null) {
-					aux = ingredientes[i + 1];
-					this.remover(ingredientes[i + 1].getNome());
-					this.ingredientes[i] = aux;
-				} else if (nome.contentEquals(ingredientes[i].getNome())) {
-					this.ingredientes[i] = null;
-					this.indice--;
-					i = 1000;
-				}
-			} else {
-				this.indice--;
-				this.ingredientes[i] = null;
-			}
-		}
+	if(this.existe(nome)) {
+		
+		int i = this.getIndiceOf(nome);
+		this.indice = this.indice -1;
+		
+		this.ingredientes[i] = this.ingredientes[this.indice];
+		this.ingredientes[this.indice] = null;
+		
+	} else {
+		throw new IngredienteNaoEncontradoException();
+	}
 	}
 
 	public Ingrediente procurar(String nome) throws IngredienteNaoEncontradoException {
-		int aux = 0;
-		for (int i = 0; i < 1000; i++) {
-			if (ingredientes[i] != null) {
-				if (ingredientes[i].getNome().contentEquals(nome)) {
-					aux = i;
-					i = 1000;
-				} 
-			} else {
-				throw new IngredienteNaoEncontradoException();
-			}
+		if(this.existe(nome)) {
+			
+			int i = this.getIndiceOf(nome);
+			Ingrediente retorno = null;
+			retorno = this.ingredientes[i];
+			
+			return retorno;
+			
+		} else {
+			throw new IngredienteNaoEncontradoException();
 		}
-		return this.ingredientes[aux];
 	}
 
 	public boolean existe(String nome) {
-		boolean existe = false;
-		for (int i = 0; i < 1000; i++) {
-			if (ingredientes[i] != null) {
-				if (ingredientes[i].getNome().contentEquals(nome)) {
-					i = 1000;
-					existe = true;
-				}
-			} else {
-				existe = false;
+		
+		for (int i = 0; i < this.indice; i++) {
+			
+			if (ingredientes[i].getNome().contentEquals(nome)) {
+			return true;
 			}
-		}
-		return existe;
+	   }
+		return false;
 	}
 }
+
